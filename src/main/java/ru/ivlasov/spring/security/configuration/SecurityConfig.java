86bc9b26221d45bd7259e9dfbc21e1372c6b71b6
eRqa <ivlasov.spb@gmail.com>
2021-10-18 23:30:21 +0300

    Хранение пользователей вынесено в БД

--- src/main/java/ru/ivlasov/spring/security/configuration/SecurityConfig.java
+++ src/main/java/ru/ivlasov/spring/security/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.ivlasov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,25 +8,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("pavel")
+//                        .password("pavel")
+//                        .roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("boris")
+//                        .password("boris")
+//                        .roles("HR"))
+//                .withUser(userBuilder.username("Ivan")
+//                        .password("Ivan")
+//                        .roles("HR", "MANAGER")
+//                );
 
+        auth.jdbcAuthentication().dataSource(dataSource);
 
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("pavel")
-                        .password("pavel")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder.username("boris")
-                        .password("boris")
-                        .roles("HR"))
-                .withUser(userBuilder.username("Ivan")
-                        .password("Ivan")
-                        .roles("HR", "MANAGER")
-                );
     }
 
     @Override
